d2e0d828854341fcb7e609126547b89744632fef
hiinng48 <hiinnguyen448@outlook.com>
2025-12-16 01:37:23 +0700

    adding graph and adjust bound for ex1

--- algorithms/Frank-Wolfe/src/problems.py
+++ algorithms/Frank-Wolfe/src/problems.py
@@ -26,8 +26,8 @@ class Example1:
         # Frank-Wolfe LMO
         # Ràng buộc: -x1^2 - 2x1x2 <= -4
         cons = ({'type': 'ineq', 'fun': lambda s: s[0]**2 + 2*s[0]*s[1] - 4})
-        bnds = [(0, 100)] * self.n
-        # Mẹo: start point = current_x giúp solver chạy nhanh hơn
+        bnds = [(0, 5)] * self.n
+        # start point = current_x giúp solver chạy nhanh hơn
         res = minimize(lambda s: np.dot(grad, s), current_x, bounds=bnds, constraints=cons, method='SLSQP')
         return res.x
 
